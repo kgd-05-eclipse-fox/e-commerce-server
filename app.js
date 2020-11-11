07fd1537da1ca91cfbabe43af161102418758271
jeplindm <jeplindm@github.com>
2020-11-11 20:54:16 +0700

    setup cors

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const route = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandler')
+const cors = require('cors');
 
 //body parser
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(cors())
 
 app.use(route);
 
 app.use(errorHandler)
 
-
-// app.listen(port, () => {
-//     console.log(`listening to the port ${port}`);
-// })
+app.listen(port, () => {
+    console.log(`listening to the port ${port}`);
+})
 
 module.exports = app;
