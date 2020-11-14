eb10a5b8943a9aece936c9f3621141a7a2753ade
abrarnaim015 <aabrarnaim24@gmail.com>
2020-11-14 14:59:23 +0700

    proses push heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if(process.env.NODE_ENV !== 'production'){
 
 const express = require('express')
 const app = express()
-const port = process.env.PROT || 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 const routers = require('./routers')
 const errorHandler = require('./middleware/errorHandler.js')
