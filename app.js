8df8e7d5d778f468b456df68e84c021fc6a5060d
yopikurniawan <yopikprabowo@gmail.com>
2020-11-14 17:21:48 +0700

    deploy server heroku

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// if (process.env.NODE_ENV == 'development') {
-  // require('dotenv').config()
-// }
-require('dotenv').config()
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const app = express()
 const router = require('./routes')
 const cors = require('cors')
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 const errorHandling = require('./middlewares/errorHandling')
 
 app.use(cors())
@@ -15,8 +14,8 @@ app.use(express.json())
 app.use(router)
 app.use(errorHandling)
 
-if (process.env.NODE_ENV !== 'test') {
-	app.listen(PORT, () => console.log(`my PORT : ${PORT}`))
-} else {
-	module.exports = app
-}
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+module.exports = app
