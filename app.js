f95fe7a77b75f10e39a05e74c7d8327389e5f790
farhanhrywn <farhanharyawan@gmail.com>
2020-11-14 16:13:55 +0700

    Update port in app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if(process.env.NODE_ENV != "production") {
 
 const express = require('express')
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 const cors = require('cors')
 const router = require('./routes')
 
