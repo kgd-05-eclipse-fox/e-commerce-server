0c2738638b2fe1b16c5a020da942d3ebac105abb
jeplindm <jeplindm@github.com>
2020-11-14 14:18:21 +0700

    fixing routing

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const UserController = require('../controllers/UserController');
 const productRoutes = require('./product');
 
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 router.post('/register', UserController.register);
 router.post('/login/admin', UserController.loginAdmin);
 router.post('/login/customer', UserController.loginCustomer);
