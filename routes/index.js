2a85063904115e136605ac1aedc190e606a9c0fe
bimakrishna <krishna.bima@yahoo.com>
2020-11-14 16:33:44 +0700

    e-commerce

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const AdminController = require('../controllers/adminController');
+const productsRoutes = require('./product.js');
+const categoriesRoutes = require('./category.js');
+const bannersRoutes = require('./banner.js');
+
+router.post('/adminRegister', AdminController.register);
+router.post('/adminLogin', AdminController.login);
+
+router.use('/products', productsRoutes);
+router.use('/categories', categoriesRoutes);
+router.use('/banners', bannersRoutes);
+
+module.exports = router;
